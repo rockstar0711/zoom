77299419771007afaf99ba2ec1a25f226186757f
rockstar0711 <happpybopy123@outlook.com>
2021-09-04 12:24:32 +0300

    frontend setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
 
 const app = express();
+const PORT = 3000;
 
-console.log('hello')
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views')
 
-app.listen(3000);
+app.use('/public', express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => res.render('home'));
+
+const handleListen = () => console.log(`Listening on http://localhost:${PORT}`)
+
+app.listen(PORT, handleListen);
